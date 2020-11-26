31518069bfc69d9ad2f8454681326b4d55476890
1703693941XQJ <1703693941xqj@kuaishou.com>
2020-11-26 22:34:12 +0800

    处理用户没登陆，而用户详情页面显示错误的bug

--- service/service_edu/src/main/java/com/jc/edu/controller/front/CourseFrontController.java
+++ service/service_edu/src/main/java/com/jc/edu/controller/front/CourseFrontController.java
@@ -1,12 +1,14 @@
 package com.jc.edu.controller.front;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.jc.edu.client.OrderClient;
 import com.jc.edu.entity.Course;
 import com.jc.edu.entity.chapter.ChapterVo;
 import com.jc.edu.entity.frontvo.CourseFrontVo;
 import com.jc.edu.entity.frontvo.CourseWebVo;
 import com.jc.edu.service.ChapterService;
 import com.jc.edu.service.CourseService;
+import com.jc.utils.JwtUtils;
 import com.jc.utils.Result;
 import com.jc.utils.ordervo.CourseWebVoOrder;
 import org.springframework.beans.BeanUtils;
@@ -14,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -36,6 +39,9 @@ public class CourseFrontController {
     @Autowired
     private ChapterService chapterService;
 
+    @Autowired
+    private OrderClient orderClient;
+
     //1 条件查询带分页查询课程
     @PostMapping("getFrontCourseList/{page}/{limit}")
     public Result getFrontCourseList(@PathVariable long page, @PathVariable long limit,
@@ -48,15 +54,22 @@ public class CourseFrontController {
 
     //课程详情方法
     @GetMapping("getFrontCourseInfo/{courseId}")
-    public Result getFrontCourseInfo(@PathVariable String courseId) {
+    public Result getFrontCourseInfo(@PathVariable String courseId, HttpServletRequest request) {
         //根据课程id，编写sql语句查询课程信息
         CourseWebVo courseWebVo = courseService.getBaseCourseInfo(courseId);
         //根据课程id查询章节和小节
         List<ChapterVo> chapterVideoList = chapterService.getChapterVideoByCourseId(courseId);
-        return Result.ok().data("courseWebVo", courseWebVo).data("chapterVoList", chapterVideoList);
+        //根据courseId和memberId来查询用户是否购买了课程
+        String memberId = JwtUtils.getMemberIdByJwtToken(request);
+        //TODO 要处理用户没登陆，而用户详情页面显示错误的bug
+        if (memberId == null||memberId.equals("")) {
+            return Result.ok().data("courseWebVo", courseWebVo).data("chapterVoList", chapterVideoList);
+        }
+        boolean isBuy = orderClient.getOrderState(courseId, memberId);
+        return Result.ok().data("courseWebVo", courseWebVo).data("chapterVoList", chapterVideoList).data("isBuy", isBuy);
     }
 
-    //课程详情方法
+    //获得课程详情方法
     @GetMapping("getCourseInfoOrder/{courseId}")
     public CourseWebVoOrder getCourseInfoOrder(@PathVariable String courseId) {
         CourseWebVo courseWebVo = courseService.getBaseCourseInfo(courseId);
