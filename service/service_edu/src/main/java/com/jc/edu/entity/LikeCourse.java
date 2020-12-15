e389ad57f5d7edde2e24d6fdb29f52aa59245567
1703693941XQJ <1703693941xqj@kuaishou.com>
2020-12-16 00:32:31 +0800

    增加用户点赞课程功能(还未实现)

---
+++ service/service_edu/src/main/java/com/jc/edu/entity/LikeCourse.java
@@ -0,0 +1,52 @@
+package com.jc.edu.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+
+import java.util.Date;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ * @deprecated 用户点赞课程的实体类
+ * @author jc
+ * @since 2020-12-16
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("user_like_course")
+@ApiModel(value="LikeCourse对象", description="用户点赞课程的实体类")
+public class LikeCourse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "每条记录的ID")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    @ApiModelProperty(value = "用户Id")
+    private Integer userId;
+
+    @ApiModelProperty(value = "课程Id")
+    private Integer courseId;
+
+    @ApiModelProperty(value = "逻辑删除")
+    private Boolean deleted;
+
+    @ApiModelProperty(value = "创建时间")
+    @TableField(fill = FieldFill.INSERT)
+    private Date gmtCreate;
+
+    @ApiModelProperty(value = "更新时间")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date gmtModified;
+
+
+}
